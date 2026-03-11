eb3eec1458b3b79aeaaa8c5d08ee2d31780955b2
Abhinaba <abhinabadas94348@gmail.com>
2026-03-11 14:30:01 +0530

    Update cf.js

--- api/cf.js
+++ api/cf.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
-  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-cf-token, x-cf-zone');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { path } = req.query;
